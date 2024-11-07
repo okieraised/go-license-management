870adb5d941667d41ff57ef8f931b9c8f7454454
tripg <phamtri2895@gmail.com>
2024-11-07 17:55:08 +0700

    update utility

--- internal/infrastructure/database/entities/product.go
+++ internal/infrastructure/database/entities/product.go
@@ -14,9 +14,9 @@ type Product struct {
 	Name                 string                 `bun:"name,type:varchar(256)"`
 	DistributionStrategy string                 `bun:"distribution_strategy,type:varchar(128)"`
 	Code                 string                 `bun:"code,type:varchar(128),unique"`
+	URL                  string                 `bun:"url,type:varchar(1024)"`
 	Platforms            []string               `bun:"platform,type:jsonb"`
 	Metadata             map[string]interface{} `bun:"metadata,type:jsonb"`
-	URL                  string                 `bun:"url,type:varchar(1024)"`
 	CreatedAt            time.Time              `bun:"created_at,nullzero,notnull,default:current_timestamp"`
 	UpdatedAt            time.Time              `bun:"updated_at,nullzero,notnull,default:current_timestamp"`
 	Tenant               *Tenant                `bun:"rel:belongs-to,join:tenant_id=id"`
