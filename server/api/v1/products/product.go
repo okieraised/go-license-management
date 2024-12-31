c1514f1fd43d8d044c37db5a883354fe52816b7a
tripg <phamtri2895@gmail.com>
2024-12-31 14:54:06 +0700

    Update product permissions

--- server/api/v1/products/product.go
+++ server/api/v1/products/product.go
@@ -46,11 +46,11 @@ func (r *ProductRouter) Routes(engine *gin.RouterGroup, path string) {
 	{
 		routes = routes.Group("/products")
 		routes.POST("", middlewares.JWTValidationMW(), middlewares.PermissionValidationMW(constants.ProductCreate), r.create)
-		routes.GET("", middlewares.JWTValidationMW(), r.list)
-		routes.GET("/:product_id", middlewares.JWTValidationMW(), r.retrieve)
-		routes.PATCH("/:product_id", middlewares.JWTValidationMW(), r.update)
-		routes.DELETE("/:product_id", middlewares.JWTValidationMW(), r.delete)
-		routes.POST("/:product_id/tokens", middlewares.JWTValidationMW(), r.tokens)
+		routes.GET("", middlewares.JWTValidationMW(), middlewares.PermissionValidationMW(constants.ProductRead), r.list)
+		routes.GET("/:product_id", middlewares.JWTValidationMW(), middlewares.PermissionValidationMW(constants.ProductRead), r.retrieve)
+		routes.PATCH("/:product_id", middlewares.JWTValidationMW(), middlewares.PermissionValidationMW(constants.ProductUpdate), r.update)
+		routes.DELETE("/:product_id", middlewares.JWTValidationMW(), middlewares.PermissionValidationMW(constants.ProductDelete), r.delete)
+		routes.POST("/:product_id/tokens", middlewares.JWTValidationMW(), middlewares.PermissionValidationMW(constants.ProductTokensGenerate), r.tokens)
 	}
 }
 
